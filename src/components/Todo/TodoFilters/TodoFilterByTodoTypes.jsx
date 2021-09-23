e672b67708a27d30eb01e1e826ed0ef89d602369
xNousex <damien.rambourg@gmail.com>
2021-09-23 11:43:49 +0200

    Fix some styles

--- src/components/Todo/TodoFilters/TodoFilterByTodoTypes.jsx
+++ src/components/Todo/TodoFilters/TodoFilterByTodoTypes.jsx
@@ -2,15 +2,15 @@ import React, { useEffect } from 'react';
 import { useQuery } from '@apollo/client';
 import { useContext } from 'react/cjs/react.development';
 import { GET_TODO_TYPES } from '../../../models/gql/queries/Todo.gql';
-import { css } from '@emotion/react';
 import { TodoContext } from '../../../contexts/TodoContext';
+import { FilterCheckBox, FilterCheckBoxContainer } from './TodoFilters.style';
 
 // Apply todo types filters
 const TodoFilterByTodoTypes = () => {
   const { todoContext, dispatch } = useContext(TodoContext);
   const { error, data } = useQuery(GET_TODO_TYPES);
 
-   // Refresh on context updated
+  // Refresh on context updated
   useEffect(() => {}, [todoContext.filters]);
 
   useEffect(() => {
@@ -40,20 +40,10 @@ const TodoFilterByTodoTypes = () => {
   if (error) return `Error! ${error.message}`;
 
   return (
-    <div
-      className={css`
-        display: flex;
-        flex-wrap: wrap;
-      `}
-    >
+    <FilterCheckBoxContainer>
       {data?.__type?.enumValues &&
         data.__type.enumValues.map((todoType, index) => (
-          <div
-            key={`filter-todo-type_${index}`}
-            className={css`
-              margin: 5px 10px;
-            `}
-          >
+          <FilterCheckBox key={`filter-todo-type_${index}`}>
             <input
               type='checkbox'
               id={`todo-type_${index}`}
@@ -66,9 +56,9 @@ const TodoFilterByTodoTypes = () => {
               }
             />
             <label htmlFor={`todo-type_${index}`}>{`${todoType.name}`}</label>
-          </div>
+          </FilterCheckBox>
         ))}
-    </div>
+    </FilterCheckBoxContainer>
   );
 };
 
---
+++ src/components/Todo/TodoFilters/TodoFilters.style.js
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+export const FilterCheckBoxContainer = styled.div(
+  `    
+  display: flex;
+  flex-wrap: wrap;
+    `
+);
+export const FilterCheckBox = styled.div(
+  `    
+    margin: 5px 10px;
+  `
+);
